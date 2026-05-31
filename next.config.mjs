00621071f67ff4eaee4432eb0665484e8f95d0b4
jieun Kim <jieunkim7912@gmail.com>
2026-05-31 23:16:49 +0900

    Convert app to PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import withPWA from 'next-pwa'
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/firebasestorage\.googleapis\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'ai-images',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig)
